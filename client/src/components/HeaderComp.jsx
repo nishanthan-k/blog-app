450feac840542750a9ffe0a6aa7aa032f17456f8
nishanthan-k <krn.nishanthan@gmail.com>
2024-06-18 18:23:18 +0530

    fix: add cursor pointer to header theme icon

--- client/src/components/HeaderComp.jsx
+++ client/src/components/HeaderComp.jsx
@@ -5,11 +5,14 @@ import { GlobalContext } from "../globals/contexts/GlobalContext";
 
 const HeaderComp = () => {
   const { theme, updateTheme } = useContext(GlobalContext);
-  console.log(theme);
+
   return (
     <header className="flex items-center justify-between px-2 h-20 shadow-md md:px-10">
       <h2 className="text-lg md:text-2xl md:font-medium">Blog App</h2>
-      <section className="flex items-center gap-4" onClickCapture={updateTheme}>
+      <section
+        className="flex items-center gap-4 cursor-pointer"
+        onClickCapture={updateTheme}
+      >
         {theme === "light" ? (
           <MdLightMode size={25} />
         ) : (
